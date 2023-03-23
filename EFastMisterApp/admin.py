efbc7e1d0b67d05f064c2c0e39a4431aa46a5046
pedro <pgarciajimenez@safareyes.es>
2023-03-23 17:06:06 +0100

    admin relleno

--- EFastMisterApp/admin.py
+++ EFastMisterApp/admin.py
@@ -2,5 +2,9 @@ from django.contrib import admin
 
 from .models import *
 # Register your models here.
-admin.site.register(Comunidad)
-admin.site.register(GestionComunidad)
+admin.site.register(Comunidad),
+admin.site.register(GestionComunidad),
+admin.site.register(AccesoComunidades),
+admin.site.register(Equipos),
+admin.site.register(Jugadores)
+
